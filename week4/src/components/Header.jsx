ac4ec94eb8d5b0a74573350cab6f47cb47a4c8bb
Dev-jwJeong <rgn95652@gmail.com>
2022-11-10 00:50:21 +0900

    Feat : 검색, 유저 리스트 api 연동

--- week4/src/components/Header.jsx
+++ week4/src/components/Header.jsx
@@ -1,21 +1,31 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionItem,
-  AccordionIcon,
-  AccordionPanel,
-  Heading,
-  Box,
-} from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Heading, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Input } from '@chakra-ui/react';
+import { getUserList } from '../api/githubAPI';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const [users, setUsers] = useState([]);
+  const [inputs, setInputs] = useState('');
+  const [usersList, setUsersList] = useState([]);
+  const navigate = useNavigate();
+
+  const onChange = e => {
+    setInputs(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    navigate(`/search/${inputs}`);
+    setInputs('');
+  };
+
+  const onMoveUser = username => {
+    navigate(`/search/${username}`);
+  };
+
   useEffect(() => {
     const getGithub = async () => {
-      const res = await axios.get('/api/products');
-      setUsers(res.data.items);
+      const res = await getUserList();
+      setUsersList(res);
     };
     getGithub();
   }, []);
@@ -23,27 +33,16 @@ const Header = () => {
   return (
     <Box>
       <Heading>Github Profile Finder</Heading>
-      <Accordion allowToggle>
-        <AccordionItem>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  누구꺼 볼래?
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              {users.map(user => (
-                <Box key={user.id} flex="1" textAlign="left">
-                  {user.user}
-                </Box>
-              ))}
-            </AccordionPanel>
-          </AccordionItem>
-        </AccordionItem>
-      </Accordion>
+      <form onSubmit={onSubmit}>
+        <Input placeholder="다른 사람 찾을래?" value={inputs} onChange={onChange} />
+      </form>
+      <Breadcrumb>
+        {usersList.map(user => (
+          <BreadcrumbItem key={user.id}>
+            <BreadcrumbLink onClick={() => onMoveUser(user.github)}>{user.user}</BreadcrumbLink>
+          </BreadcrumbItem>
+        ))}
+      </Breadcrumb>
     </Box>
   );
 };
