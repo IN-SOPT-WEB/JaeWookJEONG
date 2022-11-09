29c8241cbd96bd2ab88357d3e864944f3e0e0c25
Dev-jwJeong <rgn95652@gmail.com>
2022-11-10 00:49:06 +0900

    Feat : 깃험 사용자 검색 api 연동

--- week4/src/components/Content.jsx
+++ week4/src/components/Content.jsx
@@ -1,28 +1,46 @@
 import { Box, Button, CloseButton, Flex, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getUserInfo } from '../api/githubAPI';
 
 const Content = () => {
+  const [user, setUser] = useState({});
+  const { username } = useParams();
+
+  useEffect(() => {
+    const onGetUser = async username => {
+      const res = await getUserInfo(username);
+      setUser(res);
+    };
+
+    onGetUser(username);
+  }, [username]);
+
+  console.log(user);
+
   return (
     <Box>
-      <CloseButton />
-      <Image fallbackSrc="https://via.placeholder.com/150" />
+      <CloseButton onClick={() => window.location.replace('/search')} />
+      <Image fallbackSrc="https://via.placeholder.com/150" src={user.avatar_url} />
       <Text as="b" fontSize="2xl">
-        이름
+        {user?.login}
       </Text>
-      <Text as="b">닉네임</Text>
-      <Button variant="outline">visit</Button>
+      <Text as="b">{user?.name}</Text>
+      <Button variant="outline" onClick={() => window.open(user.html_url, '_blank')}>
+        visit
+      </Button>
       <Flex justifyContent="space-between">
         <Box w="30%">
           <Text>Followers</Text>
-          <Text>2</Text>
+          <Text>{user?.followers}</Text>
         </Box>
         <Box w="30%">
           <Text>Following</Text>
-          <Text>2</Text>
+          <Text>{user?.following}</Text>
         </Box>
         <Box w="30%">
           <Text>Repos</Text>
-          <Text>2</Text>
+          <Text>{user?.public_repos}</Text>
         </Box>
       </Flex>
     </Box>
