f589a0bd6be9a90b545ccb07de7da308219f1b2b
Dev-jwJeong <rgn95652@gmail.com>
2022-11-10 01:45:34 +0900

    Fix : Layout 컴포넌트 적용

--- week4/src/components/Content.jsx
+++ week4/src/components/Content.jsx
@@ -2,6 +2,7 @@ import { Box, Button, CloseButton, Flex, Image, Text } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getUserInfo } from '../api/githubAPI';
+import Layout from './Layout';
 
 const Content = () => {
   const [user, setUser] = useState({});
@@ -19,31 +20,35 @@ const Content = () => {
   console.log(user);
 
   return (
-    <Box>
-      <CloseButton onClick={() => window.location.replace('/search')} />
-      <Image fallbackSrc="https://via.placeholder.com/150" src={user.avatar_url} />
-      <Text as="b" fontSize="2xl">
-        {user?.login}
-      </Text>
-      <Text as="b">{user?.name}</Text>
-      <Button variant="outline" onClick={() => window.open(user.html_url, '_blank')}>
-        visit
-      </Button>
-      <Flex justifyContent="space-between">
-        <Box w="30%">
-          <Text>Followers</Text>
-          <Text>{user?.followers}</Text>
-        </Box>
-        <Box w="30%">
-          <Text>Following</Text>
-          <Text>{user?.following}</Text>
-        </Box>
-        <Box w="30%">
-          <Text>Repos</Text>
-          <Text>{user?.public_repos}</Text>
-        </Box>
-      </Flex>
-    </Box>
+    <Layout>
+      <Box w="300px">
+        <Flex m={4} flexDirection="column" alignItems="center" justifyContent="center">
+          <CloseButton onClick={() => window.location.replace('/search')} />
+          <Image fallbackSrc="https://via.placeholder.com/150" src={user.avatar_url} w={150} />
+          <Text as="b" fontSize="2xl">
+            {user?.login}
+          </Text>
+          <Text as="b">{user?.name}</Text>
+          <Button variant="outline" onClick={() => window.open(user.html_url, '_blank')}>
+            visit
+          </Button>
+          <Flex w="100%" justifyContent="space-between">
+            <Box>
+              <Text>Followers</Text>
+              <Text>{user?.followers}</Text>
+            </Box>
+            <Box>
+              <Text>Following</Text>
+              <Text>{user?.following}</Text>
+            </Box>
+            <Box>
+              <Text>Repos</Text>
+              <Text>{user?.public_repos}</Text>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
+    </Layout>
   );
 };
 
--- week4/src/components/Header.jsx
+++ week4/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Heading, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Input } from '@chakra-ui/react';
 import { getUserList } from '../api/githubAPI';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
+import Layout from './Layout';
 
 const Header = () => {
   const [inputs, setInputs] = useState('');
@@ -31,19 +32,22 @@ const Header = () => {
   }, []);
 
   return (
-    <Box>
-      <Heading>Github Profile Finder</Heading>
-      <form onSubmit={onSubmit}>
-        <Input placeholder="다른 사람 찾을래?" value={inputs} onChange={onChange} />
-      </form>
-      <Breadcrumb>
-        {usersList.map(user => (
-          <BreadcrumbItem key={user.id}>
-            <BreadcrumbLink onClick={() => onMoveUser(user.github)}>{user.user}</BreadcrumbLink>
-          </BreadcrumbItem>
-        ))}
-      </Breadcrumb>
-    </Box>
+    <Layout>
+      <Box>
+        <Heading>Github Profile Finder</Heading>
+        <form onSubmit={onSubmit}>
+          <Input placeholder="다른 사람 찾을래?" value={inputs} onChange={onChange} />
+        </form>
+        <Breadcrumb>
+          {usersList.map(user => (
+            <BreadcrumbItem key={user.id}>
+              <BreadcrumbLink onClick={() => onMoveUser(user.github)}>{user.user}</BreadcrumbLink>
+            </BreadcrumbItem>
+          ))}
+        </Breadcrumb>
+      </Box>
+      <Outlet />
+    </Layout>
   );
 };
 
