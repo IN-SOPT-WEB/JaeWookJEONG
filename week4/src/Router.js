3781fabccbf6959f4600a7045f84a67c79583964
Dev-jwJeong <rgn95652@gmail.com>
2022-11-10 01:45:52 +0900

    Fix : url 컴포넌트 변경

--- week4/src/Router.js
+++ week4/src/Router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './components/Main';
+import Content from './components/Content';
+import Header from './components/Header';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/search" element={<Main />}>
-          <Route path=":username" element={<Main />} />
+        <Route path="/search" element={<Header />}>
+          <Route path=":username" element={<Content />} />
         </Route>
       </Routes>
     </BrowserRouter>
