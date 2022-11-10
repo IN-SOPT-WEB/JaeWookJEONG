4f3f11664cde305fc827f37742890f1fd5f2a39e
Dev-jwJeong <rgn95652@gmail.com>
2022-11-10 21:57:10 +0900

    Fix : github api 요청 초과로 인한 토큰 추가

--- week4/src/api/customAPI.js
+++ week4/src/api/customAPI.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const token = process.env.REACT_APP_TOKEN;
+
 export const instance = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
     'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
   },
 });
