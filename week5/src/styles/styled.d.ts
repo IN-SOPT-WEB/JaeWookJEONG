51e429f83a3da3d24a3a103a3130bf44f88f6186
Dev-jwJeong <rgn95652@gmail.com>
2022-12-24 16:51:16 +0900

    Feat : 스타일 타입 지정

---
+++ week5/src/styles/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      ivory: string;
+      green: string;
+      red: string;
+    };
+  }
+}
