cc672715fe662231dc49cadcc54fa55261df55de
Dev-jwJeong <rgn95652@gmail.com>
2022-12-24 16:51:28 +0900

    Css : 컬러 저장

---
+++ week5/src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  ivory: '#FCFFE7',
+  green: '#008000',
+  red: '#FF0000',
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+
+export default theme;
