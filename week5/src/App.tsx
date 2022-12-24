1e4ce98bf2d6a16fd7d5383446cb3501f85a63a8
Dev-jwJeong <rgn95652@gmail.com>
2022-12-24 16:52:26 +0900

    Feat : 글로벌 스타일, 테마 전역 설정

--- week5/src/App.tsx
+++ week5/src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import Router from './lib/Router';
+import GlobalStyles from './styles/globalStyle';
+import theme from './styles/theme';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default App;
