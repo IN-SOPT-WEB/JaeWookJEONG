3dc3b7d7dda13c6a7231325339986651bb30744e
Dev-jwJeong <rgn95652@gmail.com>
2022-12-24 17:51:51 +0900

    Feat : msw 설정

--- week5/src/index.tsx
+++ week5/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
