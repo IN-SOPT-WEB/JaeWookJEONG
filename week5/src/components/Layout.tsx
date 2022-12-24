40b2c68f336174fe4423604405b8c46d52b71e26
Dev-jwJeong <rgn95652@gmail.com>
2022-12-24 16:52:55 +0900

    Css : 공통 레이아웃 컴포넌트

---
+++ week5/src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface MyChildrenProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: MyChildrenProps) => {
+  return <Styled.Root>{children}</Styled.Root>;
+};
+
+export default Layout;
+
+const Styled = {
+  Root: styled.div`
+    width: 1440px;
+    padding: 0 1rem;
+  `,
+};
