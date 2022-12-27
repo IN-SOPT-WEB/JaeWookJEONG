3c7d1af031faf24e383070e25bfa03887b4fd84c
Dev-jwJeong <rgn95652@gmail.com>
2022-12-27 18:25:34 +0900

    Feat : 편지 쓰기 api 작성

--- week5/src/mocks/handlers.ts
+++ week5/src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { LetterProps } from '../types';
 
 const letters = [
   {
-    name: '정재욱',
+    username: '정재욱',
     password: '1129',
     content: '안녕하세요?',
     hint: '생일',
@@ -11,19 +11,21 @@ const letters = [
 ];
 const handlers = [
   rest.get('/api/letters', async (req, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(400), ctx.json(letters));
+    return res(ctx.status(200), ctx.delay(400), ctx.json(localStorage.getItem('letters')));
   }),
   rest.post<LetterProps>('/api/write', async (req, res, ctx) => {
-    const { name, password, content, hint } = await req.json();
+    const { username, password, content, hint } = await req.json();
 
     const newLetter = {
-      name,
+      username,
       password,
       content,
       hint,
     };
 
-    letters.push(newLetter);
+    const text = localStorage.getItem('letters');
+    localStorage.setItem('letters', JSON.stringify(letters));
+
     return res(ctx.status(200), ctx.delay(400));
   }),
 ];
