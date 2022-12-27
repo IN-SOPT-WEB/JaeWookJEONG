f5ef891b46c51a9408b883e5ca453d256c5bc715
Dev-jwJeong <rgn95652@gmail.com>
2022-12-27 20:09:53 +0900

    Feat : 편지 상세보기 페이지 url

--- week5/src/lib/Router.tsx
+++ week5/src/lib/Router.tsx
@@ -1,6 +1,7 @@
 import LetterPage from '../pages/LetterPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WritePage from '../pages/WritePage';
+import ContentPage from '../pages/ContentPage';
 
 const router = () => {
   return (
@@ -8,6 +9,7 @@ const router = () => {
       <Routes>
         <Route path="/" element={<LetterPage />} />
         <Route path="/write" element={<WritePage />} />
+        <Route path="/content/:letterId" element={<ContentPage />} />
       </Routes>
     </BrowserRouter>
   );
