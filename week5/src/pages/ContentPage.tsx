d2cb18c40df6f92aeac8a229ace67cbcccb7988f
Dev-jwJeong <rgn95652@gmail.com>
2022-12-27 20:10:41 +0900

    Feat : 편지 상세보기/ 힌트에 따른 기능 구현

---
+++ week5/src/pages/ContentPage.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { StyledInput } from '../components/Input';
+import Layout from '../components/Layout';
+import { useGetLetterHook } from '../hooks/useGetLetterHook';
+
+const ContentPage = () => {
+  const [input, setInput] = useState('');
+  const [IntLetterId, setIntLetterId] = useState<number>(0);
+  const [check, setCheck] = useState<boolean>(false);
+  const { letterId } = useParams();
+  const { letters } = useGetLetterHook();
+
+  useEffect(() => {
+    if (letterId === undefined) return;
+    setIntLetterId(parseInt(letterId));
+  }, [letterId]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (letterId === undefined) return;
+    if (input === letters?.[IntLetterId].password) {
+      setCheck(true);
+    } else {
+      setCheck(false);
+    }
+  };
+
+  return (
+    <Layout>
+      {check === false ? (
+        <Styled.CheckPassword>
+          <Styled.PasswordForm onSubmit={onSubmit}>
+            <Styled.PasswordText>hint : {letters?.[IntLetterId]?.hint}</Styled.PasswordText>
+            <StyledInput
+              fullWidth="fullWidth"
+              type="password"
+              value={input}
+              placeholder="비밀번호를 입력해주세요"
+              onChange={onChange}
+            />
+          </Styled.PasswordForm>
+        </Styled.CheckPassword>
+      ) : (
+        <Styled.Content>
+          <Styled.ContentTitle>글쓴이 : {letters?.[IntLetterId]?.username}</Styled.ContentTitle>
+          <Styled.ContentTitle> {letters?.[IntLetterId]?.content}</Styled.ContentTitle>
+        </Styled.Content>
+      )}
+    </Layout>
+  );
+};
+
+export default ContentPage;
+
+const Styled = {
+  CheckPassword: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+  PasswordText: styled.h2``,
+  PasswordInput: styled.input``,
+  PasswordForm: styled.form``,
+  Content: styled.div``,
+  ContentTitle: styled.h2``,
+  ContentText: styled.p``,
+};
